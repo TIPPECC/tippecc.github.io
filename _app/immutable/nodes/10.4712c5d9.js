7cefac680895d94dff49e18cd3433f4e05f7aa52
geofranzi <geofranzi@users.noreply.github.com>
2023-09-14 18:01:04 +0000

    deploy: 0df38feedfc7f10c2b14552a9e5588b774fec73c

--- _app/immutable/nodes/10.4712c5d9.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.b5d61e3b.js";import{S as t,i as e}from"../chunks/index.0873ccbe.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
---
+++ _app/immutable/nodes/5.e527d14c.js
@@ -0,0 +1,18 @@
+import{s as o,f as r,g as l,F as d,j as c,i as h,D as t,d as m}from"../chunks/scheduler.1919c3a5.js";import{S as u,i as p}from"../chunks/index.3e159687.js";import{l as f}from"../chunks/leadership-svgrepo-com.bd9e01c4.js";import{i as g}from"../chunks/other-income-svgrepo-com.c34770cd.js";function v(n){let e,i=`<span class="chip bg-[#D17208] mt-4"><b>Case Study 2</b></span> <div class="flex"><h1 class="content-heading">Livelihoods of communities living in semi-arid regions</h1> <img src="${g}" alt="..." width="40px"/></div> <div class="flex pt-2 pb-4"><img src="${f}" alt="Lead" width="30px"/> <div class="pl-4 text-lg"><a class="anchor" href="/team#theo_wassenaar">Theo Wassenaar</a></div></div> <blockquote class="content-blockquote">Drylands’ defining feature of water scarcity puts them at disproportionate risk to all forms of
+		land degradation. Africa, with ~75% of its surface area classified as dryland is thus
+		particularly susceptible, due to land use, social and climate change. The human populations of
+		the arid and hyper-arid zones are highly vulnerable because livelihoods here are marginal at
+		best and the human-environment systems are tightly coupled and in non-equilibrium. Several
+		observations suggest that the traditional grazing practices of pastoralist societies such as the
+		Ovahimba and Damara, developed over centuries in response to the stochastic, non-equilibrium
+		dynamics of arid rangelands with their frequent droughts, are changing. Such changes, in
+		addition to expected climate change can only exacerbate soil degradation rates, and may
+		constitute a regional tipping point. It may in fact constitute conditions for a multidomain,
+		cascading regime shift to follow. Three main uncertainties prevent a confident assessment of the
+		risk of a regime shift: 1) the extent to which the traditional Ovahimba grazing practices have
+		changed, 2) the historical, recent and likely future rates of soil degradation and how these are
+		related to grazing and restoration potential, and 3) the changes in climate and how these are
+		likely to affect soil moisture levels and thus the potential for degradation and a shift in the
+		boundaries of the arid zone. Exploring these uncertainties may lead to adaptation options, and
+		will address a more comprehensive set of vulnerabilities and responses to a changing environment
+		in the arid northwest of Namibia.</blockquote>`;return{c(){e=r("div"),e.innerHTML=i,this.h()},l(a){e=l(a,"DIV",{class:!0,"data-svelte-h":!0}),d(e)!=="svelte-1i7ep9o"&&(e.innerHTML=i),this.h()},h(){c(e,"class","content-div")},m(a,s){h(a,e,s)},p:t,i:t,o:t,d(a){a&&m(e)}}}class _ extends u{constructor(e){super(),p(this,e,null,v,o,{})}}export{_ as component};
