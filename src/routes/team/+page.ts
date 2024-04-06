d2f5559c696594dc4ad2ece7e9f595fbc373c974
Franzi <franzi82@gmx.de>
2024-04-06 11:20:02 +0200

    fix broken team link

--- src/routes/team/+page.ts
+++ src/routes/team/+page.ts
@@ -6,7 +6,9 @@ import { API_URL } from '../../app.config';
 export async function load() {
 	try {
 		// const req = await fetch(API_URL + '/layers/get_contacts_website');
-		const req = await fetch('https://leutra.geogr.uni-jena.de/backend_geoportal/climate/search_collection?variable_abbr=pr');
+		const req = await fetch(
+			API_URL + '/layers/get_contacts_website'
+		);
 
 		const result: Record<string, any>[] = await req.json();
 		console.log({ result });
