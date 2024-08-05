38c25d390ff5e6403ff8e3eda5893c4bbbf93086
geofranzi <geofranzi@users.noreply.github.com>
2024-08-05 22:12:17 +0000

    deploy: 3d980710b2c777c515aaad2a769a8db979b54200

--- _app/immutable/nodes/15.DwQtHRkD.js
+++
@@ -1,1 +0,0 @@
-import{P as m}from"../chunks/15.rav5lt11.js";export{m as component};
---
+++ _app/immutable/nodes/20.1UHajPV-.js
@@ -0,0 +1,19 @@
+import{s as n,e as r,c as l,l as c,m as d,i as h,n as a,f as m}from"../chunks/scheduler.BwrNQohB.js";import{S as p,i as f}from"../chunks/index.BXJ-9HM3.js";import{l as v}from"../chunks/leadership-svgrepo-com.DUOcY-T0.js";import{g as u}from"../chunks/group-svgrepo-com.BgWywPzX.js";import{h as g}from"../chunks/host-website-se-svgrepo-com.BYVxOZu3.js";function b(i){let e,s=`<span class="chip bg-[#98B1D8] mt-4"><b>WP 3</b></span> <div class="flex"><h1 class="content-heading">Climate Services Gateway through co-production</h1> <img src="${g}" alt="..." width="40px"/></div> <blockquote class="content-blockquote">he objective of this WP is the development of a web-based Climate Service Gateway with and for
+		scientists and stakeholders, which will serve as a platform for easy access to projections of
+		regional climate change and climate impact indicators related to water and food security and
+		biodiversity conservation.</blockquote> <div class="flex pt-10"><img src="${v}" alt="Lead" width="30px"/> <div class="pl-4 text-lg"><a class="anchor" href="/team/#sophie_biskop">Sophie Biskop (Uni Jena)</a></div></div> <div class="flex pt-4"><img src="${u}" alt="Lead" width="30px"/> <div class="pl-4 text-lg"><a class="anchor" href="/team/#uni_jena">Uni Jena</a></div> <div class="pl-4 text-lg"><a class="anchor" href="/team/#gerics">GERICS</a></div> <div class="pl-4 text-lg"><a class="anchor" href="/team/#wits_gci">WITS-GCI</a></div></div> <h2 class="h4 pt-10 pb-2">Objectives</h2>
+	The objective of this WP is the development of a web-based Climate Service Gateway with and for scientists
+	and stakeholders, which will serve as a platform for easy access to projections of regional climate
+	change and climate impact indicators related to water and food security and biodiversity conservation.
+	The services provided will be based on the need identified in the stakeholder engagement, and their
+	fitness-for-use will be assessed by ongoing engagement with the user community. Addressing the need
+	for spatially explicit and problem-tailored information on future climate, the system will provide
+	functions for accessing climate projection data (WP1) at user-defined points/areas of interest and
+	for the dynamic calculation of related climate indicators (WP2) on demand. Moreover, the system will
+	provide functions to initiate and control the processing of ensembles of projections, to manage and
+	share the outputs of user-defined data analyses, and to visualize results by means of graphs and maps.
+	These outputs will be designed with the users to ensure that the visualizations are indeed useful.
+	General metadata and detailed provenance information will be stored for each created dataset to ease
+	further use. It should be noted that a climate service such as the one to be developed here does not
+	currently exist in the SADC region, yet there is an immense demand for such a capability. An important
+	stakeholder in the co-production process to develop the Climate Services Gateway is SASSCAL itself.`;return{c(){e=r("div"),e.innerHTML=s,this.h()},l(t){e=l(t,"DIV",{class:!0,"data-svelte-h":!0}),c(e)!=="svelte-1jco5l0"&&(e.innerHTML=s),this.h()},h(){d(e,"class","content-div")},m(t,o){h(t,e,o)},p:a,i:a,o:a,d(t){t&&m(e)}}}class C extends p{constructor(e){super(),f(this,e,null,b,n,{})}}export{C as component};
