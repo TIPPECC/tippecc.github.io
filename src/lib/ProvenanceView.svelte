cca8c34f2418306762b9767d38172aee99609da5
Franzi <franzi82@gmx.de>
2025-04-28 21:40:03 +0200

    Fix links to prov sections #53

--- src/lib/ProvenanceView.svelte
+++ src/lib/ProvenanceView.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import RecursiveDisplay from '$lib/RecursiveDisplay.svelte';
 
+
 	export let metadata_prov = {};
 	export let metadata_prov_stats: {
 		count?: {
@@ -31,6 +32,8 @@
 			};
 		};
 	} = {};
+    let random_id = Math.random().toString(36).substring(2, 15); // Generate a random ID
+
 </script>
 
 {#if metadata_prov}
@@ -51,16 +54,16 @@
 						>{metadata_prov_stats?.count?.results?.bindings?.['0']?.entityCount?.value}
 
 						({metadata_prov_stats?.['source_entities']?.['results']?.['bindings']?.length}
-						<a href="#source_files" class="underline"> source files</a>
+						<a href="#source_files_{random_id}" class="underline"> source files</a>
 						&
 						{metadata_prov_stats?.['result_entities']?.['results']?.['bindings']?.length}
-						<a href="#result_files" class="underline"> result files</a>)
+						<a href="#result_files_{random_id}" class="underline"> result files</a>)
 					</span>
 				</div>
 				<div class="flex gap-2">
 					<span class="font-bold text-blue-400">Number of files based on this file:</span>
 					<span
-						><a href="#base_for_entities" class="underline"
+						><a href="#base_for_entities_{random_id}" class="underline"
 							>{metadata_prov_stats?.['base_for_entities']?.['results']?.['bindings']?.length ??
 								0}</a
 						></span
@@ -81,7 +84,7 @@
 					>
 				</div>
 				<div class="flex gap-2">
-					<span class="font-bold text-blue-400">Number of used Software:</span>
+					<span class="font-bold text-blue-400">Number of used software:</span>
 					<span
 						>{metadata_prov_stats?.count?.results?.bindings?.['0']?.softwareCount?.value ??
 							'N/A'}</span
@@ -105,7 +108,7 @@
 {/if}
 
 {#if metadata_prov_stats}
-	<div class="bg-box" id="source_files">
+	<div class="bg-box" id="source_files_{random_id}">
 		<h2 class="text-lg font-semibold mb-3 flex items-center gap-2 text-white">
 			ℹ️ Provenance: Source Files (e.g. downloaded from the ESGF portal + additional files)
 		</h2>
@@ -115,7 +118,7 @@
 			{/each}
 		</div>
 	</div>
-	<div class="bg-box" id="result_files">
+	<div class="bg-box" id="result_files_{random_id}">
 		<h2 class="text-lg font-semibold mb-3 flex items-center gap-2 text-white">
 			ℹ️ Provenance: Result Files (products of the processing chain)
 		</h2>
@@ -127,7 +130,7 @@
 			{/each}
 		</div>
 	</div>
-	<div class="bg-box" id="base_for_entities">
+	<div class="bg-box" id="base_for_entities_{random_id}">
 		<h2 class="text-lg font-semibold mb-3 flex items-center gap-2 text-white">
 			ℹ️ Provenance: Files based on this file
 		</h2>
