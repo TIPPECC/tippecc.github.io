3ad1d6d9a3ccd23cb4c00f6a4da9db07152a323e
Franzi <franzi82@gmx.de>
2025-05-06 08:02:23 +0200

    Sort prov and related dataset link lists #59

--- src/lib/ProvenanceListView.svelte
+++ src/lib/ProvenanceListView.svelte
@@ -1,9 +1,23 @@
 <script>
 	export let data = null;
+	export let only_links = false;
+
+	// split entity name and sort by name
+	const sortedData = data?.['results']?.['bindings']?.sort((a, b) => {
+		if (!b.entity || !b.entity.value) {
+			return 1; // Move to the end if no value
+		}
+		if (!a.entity || !a.entity.value) {
+			return 1; // Move to the end if no value
+		}
+		const nameA = a.entity.value.split('/').slice(-1)[0].toLowerCase();
+		const nameB = b.entity.value.split('/').slice(-1)[0].toLowerCase();
+		return nameA.localeCompare(nameB);
+	});
 </script>
 
 <div class="ml-2">
-	{#each data?.['results']?.['bindings'] ?? [] as source_file}
+	{#each sortedData ?? [] as source_file}
 		{#if source_file.entity}
 			{#if source_file['collection_id']}
 				<li class="underline">
@@ -13,7 +27,7 @@
 						target="_self">{source_file.entity.value.split('/').slice(-1)[0]}</a
 					>
 				</li>
-			{:else}
+			{:else if !only_links}
 				<li>{source_file.entity.value.split('/').slice(-1)[0]}</li>
 			{/if}
 		{/if}
