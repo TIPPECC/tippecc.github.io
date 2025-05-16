dabc573f851adb338ea08f4f90c1cc4cd45f2c11
Franzi <franzi82@gmx.de>
2025-05-16 08:08:39 +0200

    Add start page for CSG #80

--- src/lib/work_package.svelte
+++ src/lib/work_package.svelte
@@ -4,21 +4,37 @@
 	export let wp = '';
 	export let title = '';
 	export let description = '';
+	export let link = true;
 </script>
 
-<a
-	class="card p-4 m-2 border border-5 border-[{color}] hover:bg-surface-600"
-	href="/work_packages/{wp.replaceAll(' ', '').toLowerCase()}"
-	title="click to learn more"
->
-	<div class="text-left mt-[-28px]"><span class="chip bg-[{color}]"><b>{wp}</b></span></div>
-	<div class="flex">
-		<span><img src={icon} alt="..." width="40px" class="mt-2" /></span>
+{#if link == true}
+	<a
+		class="card p-4 m-2 border border-5 border-[{color}] hover:bg-surface-600"
+		href="/work_packages/{wp.replaceAll(' ', '').toLowerCase()}"
+		title="click to learn more"
+	>
+		<div class="text-left mt-[-28px]"><span class="chip bg-[{color}]"><b>{wp}</b></span></div>
+		<div class="flex">
+			<span><img src={icon} alt="..." width="40px" class="mt-2" /></span>
 
-		<b class="text-lg underline underline-offset-4 decoration-[{color}] ml-2">{title}</b><br />
-	</div>
+			<b class="text-lg underline underline-offset-4 decoration-[{color}] ml-2">{title}</b><br />
+		</div>
 
-	<blockquote class="mt-1 text-sm">
-		{description}
-	</blockquote>
-</a>
+		<blockquote class="mt-1 text-sm">
+			{description}
+		</blockquote>
+	</a>
+{:else}
+	<span class="card p-4 m-2 border border-5 border-[{color}] hover:bg-surface-600">
+		<div class="text-left mt-[-28px]"><span class="chip bg-[{color}]"><b>{wp}</b></span></div>
+		<div class="flex">
+			<span><img src={icon} alt="..." width="40px" class="mt-2" /></span>
+
+			<b class="text-lg underline underline-offset-4 decoration-[{color}] ml-2">{title}</b><br />
+		</div>
+
+		<blockquote class="mt-1 text-sm">
+			{description}
+		</blockquote>
+	</span>
+{/if}
