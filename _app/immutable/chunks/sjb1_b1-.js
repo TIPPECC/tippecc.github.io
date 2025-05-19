51f0f578fceef6533c65304c95ec4375db69c51a
geofranzi <geofranzi@users.noreply.github.com>
2025-05-19 15:08:14 +0000

    deploy: 817c07e4ed183e56f53621cda68ca6be6b441faf

--- _app/immutable/chunks/sjb1_b1-.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./Up_KV-Yx.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
