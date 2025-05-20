3fae2693351ac56e850218d16fcb4130e993474d
Fabian Schreiter <da86mej@ads.uni-jena.de>
2025-05-20 10:46:33 +0200

    update conferences #88

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -19,12 +19,69 @@
 			year: '2024',
 			title: 'Determining risk of a regime shift in a coupled arid Social-Ecological System',
 			journal: 'Journal Namibia Scientific Society',
+			publisher: 'Namibia Scientific Society',
 			volume: '70',
 			pages: '9-28',
 			doi: 'https://library.namscience.com/cgi-bin/koha/opac-detail.pl?biblionumber=97135'
-		}
+		},
 	];
 	const conferences = [
+		{
+			authors: 'Biskop S., Schreiter F., Zander F., Weber T., Remedio A., Kralisch S., Steinkopf J. and Engelbrecht F.',
+			year: '2025',
+			title: 'A Robust Signal of Soil Drying across Southern Africa in a Warmer World',
+			conference: 'International Conference on Southern Hemisphere Meteorology and Oceanography (ICSHMO)',
+			date: '31 March – 4 April 2025',
+			location: 'Cape Town, South Africa'
+		},
+		{
+			authors: 'Engelbrecht F., Steinkopf J. and Biskop S.',
+			year: '2024',
+			title: 'Can a climate-change induced megadrought bring "day-zero" to Gauteng?',
+			conference: '38th Annual Conference of South African Society for Atmospheric Sciences (SASAS)',
+			date: '29-30 October 2024',
+			location: 'Potchefstroom, South Africa'
+		},
+		{
+			authors: 'Perkins J., Remedio A., Weber T., Steinkopf J., Goikantswemang T., Buti K., Mashabane B., Montsiemang B. and Engelbrecht F.',
+			year: '2024',
+			title: 'Following the Ancient Footsteps to Survive the Anthropocene',
+			conference: '38th Annual Conference of the South African Society for Atmospheric Sciences',
+			date: '29-30 October 2024',
+			location: 'Potchefstroom, South Africa'
+		},
+		{
+			authors: 'Remedio A., Weber T., Sein D., Steinkopf J., Padvatan J., Goikantswemang T., Dhliwayo M., Perkins J. and Engelbrecht F.',
+			year: '2024',
+			title: 'Representing Historical Drought Conditions using the Latest High-Resolution Regional Climate Simulations over Southern Africa',
+			conference: '38th Annual Conference of the South African Society for Atmospheric Sciences',
+			date: '29-30 October 2024',
+			location: 'Potchefstroom, South Africa'
+		},
+		{
+			authors: 'Biskop S., Kralisch S., Lumsden T., Malherbe J., Moseki C. and Engelbrecht F.',
+			year: '2024',
+			title: 'Hydrological projections for a ‘day-zero drought’ risk assessment in the Integrated Vaal River System, South Africa',
+			conference: '9th GEWEX Open Science Conference',
+			date: '8-12 July 2024',
+			location: 'Sapporo, Japan'
+		},
+		{
+			authors: 'Biskop S. and Engelbrecht F.',
+			year: '2024',
+			title: 'Methodologies in Attribution Modelling of Hydrological Extremes',
+			conference: 'Caribbean Climate Science Workshop Series: Detection & Attribution Science Workshop',
+			date: '10-14 June 2024',
+			location: 'Kingston, Jamaica'
+		},
+		{
+			authors: 'Remedio A., Weber T., Engelbrecht F., Biskop S., Steinkopf J., Padvatan J., Van der Waal C., Wassenaar W., Banda K., Tlhalerwa K. and Perkins J.',
+			year: '2024',
+			title: 'Projected climate change signals for selected hotspot regions using regional climate simulations in southern Africa',
+			conference: 'EGU General Assembly Conference Abstracts',
+			date: '2024',
+			location: 'Vienna, Austria'
+		},
 		{
 			authors: 'Biskop S., Kralisch S., Weber T., Remedio A. and Engelbrecht F.',
 			year: '2023',
