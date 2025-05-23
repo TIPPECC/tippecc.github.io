323d3f935ff51432d05ea75dd7f6dabe3de3c477
Franzi <franzi82@gmx.de>
2025-05-23 11:09:55 +0200

    Fix OpenLayers css missing #92

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss(),cssInjectedByJsPlugin()],
+	plugins: [sveltekit(), purgeCss()],
 	server: {
 		fs: {
 			// Allow serving files from one level up to the project root
