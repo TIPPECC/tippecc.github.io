aa5014d162841525f3ed68f177abc26adbb321ce
geofranzi <franzi82@gmx.de>
2025-09-06 11:50:30 +0200

    Format Code

--- src/routes/provenance/+page.svelte
+++ src/routes/provenance/+page.svelte
@@ -1,19 +1,19 @@
 <script>
-    import { writable } from 'svelte/store';
-    import TopBar from '$lib/components/TopBar.svelte';
-    import DetailView from '$lib/components/DetailView.svelte';
-    import MinimalView from '$lib/components/MinimalView.svelte';
-    import CollectionView from '$lib/components/CollectionView.svelte';
+	import { writable } from 'svelte/store';
+	import TopBar from '$lib/components/TopBar.svelte';
+	import DetailView from '$lib/components/DetailView.svelte';
+	import MinimalView from '$lib/components/MinimalView.svelte';
+	import CollectionView from '$lib/components/CollectionView.svelte';
 
-    let activeView = writable("View 1"); // Track the active view
+	let activeView = writable('View 1'); // Track the active view
 </script>
 
 <TopBar {activeView} />
 
-{#if $activeView === "View 1"}
-    <MinimalView />
-{:else if $activeView === "View 2"}
-    <DetailView />
-{:else if $activeView === "View 3"}
-    <CollectionView />
+{#if $activeView === 'View 1'}
+	<MinimalView />
+{:else if $activeView === 'View 2'}
+	<DetailView />
+{:else if $activeView === 'View 3'}
+	<CollectionView />
 {/if}
