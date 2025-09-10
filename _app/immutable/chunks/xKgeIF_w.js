c828a8a43a0f9f1e300dc26244c2875808746357
FlatErikk <FlatErikk@users.noreply.github.com>
2025-09-10 09:12:38 +0000

    deploy: 3f62593a0bfac2de3d3728def4d1ac848d825c6d

--- _app/immutable/chunks/xKgeIF_w.js
+++
@@ -1,2 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1tmwu0i)==null?void 0:s.base)??"";var t;const e=((t=globalThis.__sveltekit_1tmwu0i)==null?void 0:t.assets)??a;export{e as a,a as b};
-//# sourceMappingURL=xKgeIF_w.js.map
