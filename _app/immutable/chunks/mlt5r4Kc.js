15997726e3a0407767c249a18ad679dff3d3ec8c
schreifab <schreifab@users.noreply.github.com>
2026-01-16 13:04:19 +0000

    deploy: 01617c5e8f55ad82802e1d1a69117eb78e8b4e23

--- _app/immutable/chunks/mlt5r4Kc.js
+++
@@ -1,2 +0,0 @@
-import{s as e}from"./Cdb_GCcr.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
-//# sourceMappingURL=mlt5r4Kc.js.map
---
+++ _app/immutable/entry/start.DKyAhK7y.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/CyNh7NjD.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.DKyAhK7y.js.map
