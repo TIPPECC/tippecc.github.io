cff66a4cc86b0304315ccd85e96a0189148be484
geofranzi <franzi82@gmx.de>
2026-02-18 22:11:33 +0100

    Add switch to change between Jena and Wits backend based on URL #142

--- src/app.config.js
+++ src/app.config.js
@@ -1,10 +1,22 @@
 import { dev } from '$app/environment';
 
 export let API_URL = '';
+export let active_backend = 'leutra';
 
 if (dev) {
-	API_URL = 'http://127.0.0.1:8000';
-	// API_URL = 'https://leutra.geogr.uni-jena.de/backend_geoportal';
-} else {
+	//API_URL = 'http://127.0.0.1:8000';
 	API_URL = 'https://leutra.geogr.uni-jena.de/backend_geoportal';
+		API_URL = 'http://sapphire.ms.wits.ac.za/backend_geoportal';
+		active_backend = 'sapphire';
+} else {
+	if (window.location.hostname === 'leutra.geogr.uni-jena.de' || window.location.hostname === 'leutra.geogr.uni-jena.de') {
+		API_URL = 'https://leutra.geogr.uni-jena.de/backend_geoportal';
+		active_backend = 'leutra';
+	} else if (window.location.hostname === 'sapphire.ms.wits.ac.za') {
+		API_URL = 'http://sapphire.ms.wits.ac.za/backend_geoportal';
+		active_backend = 'sapphire';
+	} else {
+		API_URL = 'https://leutra.geogr.uni-jena.de/backend_geoportal';
+		active_backend = 'leutra';
+	}
 }
